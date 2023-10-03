1ba115d7628472013898213f65a9b11329866e97
waelalq <wael.abuomr@gmail.com>
2023-10-03 22:32:32 +0300

    Interface segregation principle

--- ISP/app/src/main/java/ISP/App.java
+++ ISP/app/src/main/java/ISP/App.java
@@ -11,15 +11,10 @@ public class App {
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> list1=new ArrayList<Integer>();
-        list1.add(1);
-        list1.add(2);
-        list1.add(3);
-        ArrayList<Integer> list2=new ArrayList<Integer>();
-        list2.add(3);
-        list2.add(2);
-        list2.add(7);
-        System.out.println( App.intersectionBetweenTwoArray(list1,list2).toString());
+
+
+        FrontEndProjectDevelopment frontEnd= new FrontEndProjectDevelopment();
+        frontEnd.BuildFrontEnd();
 
     }
 
